c1aa2286cf6fe0b94c1bb71c6571457141b12f82
Sampath Singamsetty <sampathsingamsetty@Sampaths-MacBook-Pro.local>
2024-06-17 11:26:00 +0530

    custom settngs update...

---
+++ custom-settings.el
@@ -0,0 +1,29 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   '(("elpa" . "https://elpa.gnu.org/packages/")
+     ("elpa-devel" . "https://elpa.gnu.org/devel/")
+     ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+     ("melpa" . "https://melpa.org/packages/")
+     ("org" . "https://orgmode.org/elpa/")))
+ '(preview-scale-function 3.0)
+ '(use-package-always-defer t)
+ '(use-package-always-ensure t)
+ '(use-package-verbose t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#5a6c75" :font "AppleMyungjo" :height 1.6 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#5a6c75" :font "AppleMyungjo" :height 1.5))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#5a6c75" :font "AppleMyungjo" :height 1.3))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#5a6c75" :font "AppleMyungjo" :height 1.2))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#5a6c75" :font "AppleMyungjo" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#5a6c75" :font "AppleMyungjo"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#5a6c75" :font "AppleMyungjo"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#5a6c75" :font "AppleMyungjo"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#5a6c75" :font "AppleMyungjo")))))
