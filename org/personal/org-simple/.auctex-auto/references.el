412066e1b8167ff8f8c389cbd9948b0eb6fbd253
fpdevil <singamsetty.sampath@gmail.com>
2025-02-02 20:40:58 -0600

    ignore auto-save-list/ folder

--- org/personal/org-simple/.auctex-auto/references.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook
- "references"
- (lambda ()
-   (LaTeX-add-bibitems
-    "Shannon:2001:MTC:584091.584093"
-    "kitchin-2015-examp-effec"
-    "edgerton1972bhagavad"))
- '(or :bibtex :latex))
-
